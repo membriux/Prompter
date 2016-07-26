6483d4f2f26ce5a5e6c3070761d0b3982f243623
Gianna Chien <giannachien.gc@gmail.com>
2016-07-25 17:07:25 -0700

    modified python

--- main.py
+++ main.py
@@ -45,24 +45,8 @@ class Comment(ndb.Model):
 #For generic homepage
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    # 1. Get info from the request
-        user = users.get_current_user() #user profile
-    # 2. Logic -- interact with the database
-        user_values = {} #empty dict, used to send values
-        template = jinja_environment.get_template('Main.html')
-        if user: #if user is true
-            username = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                username, logout_url)
-            user_values = {'username':username}
-            self.response.write(template.render(user_values))
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-        # 3. Render a response
-        check_login = '<html><body>{}</body></html>'.format(greeting)
-        self.response.write('<html><body>{}</body></html>'.format(greeting))
+        template = jinja_environment.get_template("Main.html")
+        self.response.write(template.render())
 
 # Use Google's page, don't make your own
 class UserHandler(webapp2.RequestHandler):
@@ -82,26 +66,18 @@ class UserHandler(webapp2.RequestHandler):
 #After login
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
-        urlsafe_key = self.request.get('key')
-        key = ndb.Key(urlsafe=urlsafe_key)
-        prompt = key.get()
-        writings = Writing.query(Writing.prompt_key == key).order(-Writing.date).fetch()
-        template_values = {'writings':writings}
         template = jinja_environment.get_template("home.html")
-        self.response.write(template.render(template_values))
+        self.response.write(template.render())
 
-    def post(self):
-        #1. get info from Request
-        title = self.request.get('title')
-        text = self.request.get('text')
-        #2. logic (interact with database)
-        new_writing = Writing(text=text, title=title)
-        new_writing.put()
-        #3. render response
-        self.redirect('/')
+class 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),    #Opening Page
     ('/user', UserHandler),
-    ('/home', HomeHandler)     #After Login
+    ('/home', HomeHandler),     #After Login
+    ('/past_prompts', PastPromptHandler),
+    ('/create', CreateHandler),
+    ('/past_writings', PastWritingsHandler),
+    ('/my_writings', MyWritingsHandler),
+    ('/writing', WritingHandler)
 ], debug=True)
